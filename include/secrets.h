111c00b7522a540b9f6754c3d430342cfd92859f
MohammadNasrullohh <srullasrul59@gmail.com>
2026-06-17 10:13:14 +0700

    Include private secrets for transfer

--- include/secrets.h
+++ include/secrets.h
@@ -2,8 +2,10 @@
 #define SECRETS_H
 
 // Isi sebelum upload kalau mau Gemini aktif.
-#define WIFI_SSID "kudacuki"
+#define WIFI_SSID "gembot"
 #define WIFI_PASSWORD "asrul123"
+#define WS_HOST "10.110.147.251"
+#define WS_PORT 3001
 #define GEMINI_API_KEY "API_KEY_GEMINI_KAMU"
 
 #endif
